ccdfa54fd3d435b1e63ae5e1e6999bf454490820
john12321 <johnomeara72@gmail.com>
2019-01-08 21:38:37 +0000

    changed ArticleCard component to class-based component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import DeleteArticle from './DeleteArticle';
 
-const ArticleCard = ({ article, user }) => {
-  // console.log(user)
-  const {
-    // article_id,
-    author,
-    // body,
-    // comment_count,
-    created_at,
-    title,
-    topic,
-    // votes
-  } = article
+class ArticleCard extends Component {
 
-  const formattedDate = new Date(created_at).toString().slice(0, 21);
 
-  return (
-    <>
-      <br />
-      <Card>
-        <CardHeader title={title} subheader={author}>
-        </CardHeader>
-        <CardContent>
-          <Typography>
-            topic: {topic}
-          </Typography>
-          <Typography>
-            created at: {formattedDate}
-          </Typography>
-        </CardContent>
-      </Card>
-      <br />
-    </>
-  );
-};
+  render(props) {
+    const { article, user } = this.props;
+    const {
+      // article_id,
+      author,
+      // body,
+      // comment_count,
+      created_at,
+      title,
+      topic,
+      // votes
+    } = article
+
+    const formattedDate = new Date(created_at).toString().slice(0, 21);
+
+
+    return (
+      <>
+        <br />
+        <Card className="articleCard">
+          <CardHeader title={title} subheader={author}>
+          </CardHeader>
+          <CardContent>
+            <Typography variant="headline">
+              {topic}
+            </Typography>
+            {/* <Typography variant="overline">
+              {body}
+            </Typography> */}
+            <Typography>
+              created at: {formattedDate}
+            </Typography>
+          </CardContent>
+          <DeleteArticle user={user} article={article} />
+        </Card>
+        <br />
+      </>
+    );
+  }
+}
 
 export default ArticleCard;
